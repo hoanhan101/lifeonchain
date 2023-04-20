78fd37efe9f0ab91ecf40595be5ce068e2f85484
Hoanh An <hoanhan101@gmail.com>
2023-04-20 11:50:43 -0700

    Add multiple tokens mint tests

--- test/LifeOnchain.js
+++ test/LifeOnchain.js
@@ -15,6 +15,17 @@ describe("LifeOnchain", function () {
         const scriptVersion = "1.0.1";
         const contractScriptName = `${scriptName}-v${scriptVersion}`;
 
+        const traitsRarities = [
+            [500, 500, 1500, 2000, 2500, 3000],
+            [
+                125, 225, 325, 525, 525, 525, 625, 625, 625, 725, 825, 825, 825,
+                825, 925, 925,
+            ],
+            [200, 500, 700, 800, 800, 900, 1000, 1000, 1200, 1300, 1600],
+        ];
+        const traitsRaritiesHash =
+            "0x43252ab79379691f924d20ae52eae95ea7897214a8cc7f8f475ef571bc4bc022";
+
         const contentStoreContract = await (
             await ethers.getContractFactory("ContentStore")
         ).deploy();
@@ -39,13 +50,14 @@ describe("LifeOnchain", function () {
             contractScriptName,
             scriptyStorageContract.address,
             scriptyBuilderContract.address,
-            "0x43252ab79379691f924d20ae52eae95ea7897214a8cc7f8f475ef571bc4bc022"
+            traitsRaritiesHash
         );
         await lifeContract.deployed();
 
         return {
             lifeContract,
             contractScriptName,
+            traitsRarities,
         };
     }
 
@@ -65,10 +77,10 @@ describe("LifeOnchain", function () {
 
     describe("Mint", function () {
         it("Single", async function () {
-            const { lifeContract, contractScriptName } = await deploy();
+            const { lifeContract, contractScriptName, traitsRarities } =
+                await deploy();
             await expect(
-                lifeContract.mint(1, [[500,500,1500,2000,2500,3000],[125,225,325,525,525,525,625,625,625,725,825,825,825,825,925,925],[200,500,700,800,800,900,1000,1000,1200,1300,1600]], 
-                {
+                lifeContract.mint(1, traitsRarities, {
                     value: ethers.utils.parseEther("0.1"),
                 })
             ).to.be.revertedWithCustomError(lifeContract, "MintClosed");
@@ -78,8 +90,7 @@ describe("LifeOnchain", function () {
             expect(await lifeContract.totalSupply()).to.equal(0);
 
             await expect(
-                lifeContract.mint(1, [[500,500,1500,2000,2500,3000],[125,225,325,525,525,525,625,625,625,725,825,825,825,825,925,925],[200,500,700,800,800,900,1000,1000,1200,1300,1600]], 
-                {
+                lifeContract.mint(1, traitsRarities, {
                     value: ethers.utils.parseEther("0.0001"),
                 })
             ).to.be.revertedWithCustomError(lifeContract, "InsufficientFunds");
@@ -87,8 +98,7 @@ describe("LifeOnchain", function () {
             expect(await lifeContract.totalSupply()).to.equal(0);
 
             await expect(
-                lifeContract.mint(1, [[500,500,1500,2000,2500,3000],[125,225,325,525,525,525,625,625,625,725,825,825,825,825,925,925],[200,500,700,800,800,900,1000,1000,1200,1300,1600]], 
-                {
+                lifeContract.mint(1, traitsRarities, {
                     value: ethers.utils.parseEther("1"),
                 })
             );
@@ -123,46 +133,49 @@ describe("LifeOnchain", function () {
             );
         });
 
-        // it("Multiple", async function () {
-        //     const { lifeContract, contractScriptName } = await deploy();
+        it("Multiple", async function () {
+            const { lifeContract, contractScriptName, traitsRarities } =
+                await deploy();
 
-        //     await lifeContract.setMintStatus(true);
+            await lifeContract.setMintStatus(true);
 
-        //     for (let i = 0; i < 10; i++) {
-        //         console.log("multiple", i);
-        //         await expect(
-        //             lifeContract.mint({
-        //                 value: ethers.utils.parseEther("1"),
-        //             })
-        //         );
+            const toMint = 10;
+            await expect(
+                lifeContract.mint(toMint, traitsRarities, {
+                    value: ethers.utils.parseEther("1"),
+                })
+            );
+            expect(await lifeContract.totalSupply()).to.equal(toMint);
 
-        //         const tokenURI = await lifeContract.tokenURI(i);
-        //         const payload = JSON.parse(
-        //             tokenURI.split("data:application/json,")[1]
-        //         );
-        //         const content = decodeURIComponent(
-        //             decodeURIComponent(payload.animation_url)
-        //         ).split("data:text/html,")[1];
+            for (let i = 0; i < toMint; i++) {
+                console.log(`tokenURI(${i})`);
+                const tokenURI = await lifeContract.tokenURI(i);
+                const payload = JSON.parse(
+                    tokenURI.split("data:application/json,")[1]
+                );
+                const content = decodeURIComponent(
+                    decodeURIComponent(payload.animation_url)
+                ).split("data:text/html,")[1];
 
-        //         const scriptFilePath = path.join(
-        //             __dirname,
-        //             `../scripts/${contractScriptName}.js`
-        //         );
-        //         const scriptFileContent = utilities.readFile(scriptFilePath);
-        //         const final = [
-        //             "<!DOCTYPE html><html><head><title>1337c475</title></head>",
-        //             content.replace(
-        //                 '<script src="data:text/javascript;base64,"></script>',
-        //                 `<script>${scriptFileContent}</script>`
-        //             ),
-        //             "</html>",
-        //         ].join("");
+                const scriptFilePath = path.join(
+                    __dirname,
+                    `../scripts/${contractScriptName}.js`
+                );
+                const scriptFileContent = utilities.readFile(scriptFilePath);
+                const final = [
+                    "<!DOCTYPE html><html><head><title>1337c475</title></head>",
+                    content.replace(
+                        '<script src="data:text/javascript;base64,"></script>',
+                        `<script>${scriptFileContent}</script>`
+                    ),
+                    "</html>",
+                ].join("");
 
-        //         utilities.writeFile(
-        //             path.join(__dirname, `output-mutiple-${i}.html`),
-        //             final
-        //         );
-        //     }
-        // });
+                utilities.writeFile(
+                    path.join(__dirname, `output-mutiple-${i}.html`),
+                    final
+                );
+            }
+        });
     });
 });
